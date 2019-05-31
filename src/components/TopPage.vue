8a3e7d440d0c4da4a6c6feb520fb4cddeeea5f43
ryo-my-favorite <tekunsinfo@yahoo.co.jp>
2019-06-01 01:27:27 +0900

    make letter part

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -1,14 +1,42 @@
 <template>
-    <v-app>
-        <h1>Microphone Test</h1>   
-        <div>
-            <v-btn small v-on:click="startRecord">Start Record</v-btn>
-            <v-btn small v-on:click="stopRecord">Stop Record</v-btn>
-        </div>
-        <div>
-            <span>{{ recordMessage }}</span>
-        </div>
-    </v-app>
+    <v-layout>
+        <v-flex>
+            <v-app>
+                <div class="header-part">
+                    <h1>Microphone Test</h1>   
+                    <div>
+                        <v-btn small v-on:click="startRecord">Start Record</v-btn>
+                        <v-btn small v-on:click="stopRecord">Stop Record</v-btn>
+                    </div>
+                    <div>
+                        <v-flex xs12 sm3 md3>
+                            <v-text-field
+                            v-model="sendTo"
+                            placeholder="To">
+                            </v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm3 md3>
+                            <v-text-field
+                            v-model="sendFrom"
+                            placeholder="From">
+                            </v-text-field>
+                        </v-flex>
+                    </div>
+                </div>
+                <hr>
+                <div>
+                    <v-flex xs 12 sm6 offset-sm3>
+                        <v-card class="letter"
+                         min-height=360>
+                            <div class="send-to">{{ "To " + sendTo }}</div>
+                            <div class="message">{{ recordMessage }}</div>
+                            <div class="send-from">{{ "From " + sendFrom }}</div>
+                        </v-card>
+                    </v-flex>
+                </div>
+            </v-app>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
@@ -24,21 +52,24 @@
 export default {
     data (){
         return{
-            recordMessage: 'here is'
+            recordMessage: '',
+            sendTo: '',
+            sendFrom: ''
         }
     },
     methods: {
         startRecord: function(){
+            this.recordMessage = "";
             recognition.start();
-recognition.onresult = (event) => {
-        let interimTranscript = '';
-        for(let i = event.resultIndex; i< event.results.length; i++){
-            let transcript = event.results[i][0].transcript;
-            if(event.results[i].isFinal){
-                finalTranscript += transcript;
-            }else{
-                interimTranscript = transcript;
-            }
+            recognition.onresult = (event) => {
+                let interimTranscript = '';
+                for(let i = event.resultIndex; i< event.results.length; i++){
+                let transcript = event.results[i][0].transcript;
+                if(event.results[i].isFinal){
+                    finalTranscript += transcript;
+                }else{
+                    interimTranscript = transcript;
+                }
         }
         this.recordMessage = finalTranscript + " " + interimTranscript;
     }
@@ -51,5 +82,25 @@ recognition.onresult = (event) => {
 </script>
 
 <style>
-
+    .header-part{
+        margin-left: 32px;
+    }
+    .letter{
+        margin-top: 32px;
+    }
+    .message{
+        border-top: solid gray 0.5px;
+        border-bottom: solid gray 0.5px;
+        padding: 8px;
+        min-height: 320px;
+    }
+    .send-to{
+        padding-top: 8px;
+        padding-left: 8px;
+    }
+    .send-from{
+        padding-bottom: 8px;
+        padding-right: 32px;
+        text-align: right;
+    }
 </style>
